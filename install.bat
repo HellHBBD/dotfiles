121911573eb4288e5d9110febc25b4bfd12dea72
Hell HBBD <paulyu602@gmail.com>
2025-10-14 22:04:26 +0800

    add install.bat

---
+++ install.bat
@@ -0,0 +1,15 @@
+:: install scoop
+powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
+powershell -Command "irm get.scoop.sh -outfile 'install.ps1'"
+powershell -Command ".\install.ps1 -RunAsAdmin"
+
+:: install apps
+scoop install git neovim fd fzf ripgrep gcc nodejs
+
+cd %USERPROFILE%
+git clone https://github.com/HellHBBD/dotfiles.git
+
+:: 刪掉舊設定
+if exist "%USERPROFILE%\AppData\Local\nvim" rmdir /s /q "%USERPROFILE%\AppData\Local\nvim"
+
+mklink /D "%USERPROFILE%\AppData\Local\nvim" "%USERPROFILE%\dotfiles\nvim\.config\nvim"
