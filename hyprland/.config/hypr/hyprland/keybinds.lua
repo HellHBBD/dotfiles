d5f07070a48b0ba0b3f6bf685e56170f5dc0cfb5
Hell HBBD <paulyu602@gmail.com>
2026-07-26 13:53:20 +0800

    Add brightness and volume OSD

--- hyprland/.config/hypr/hyprland/keybinds.lua
+++ hyprland/.config/hypr/hyprland/keybinds.lua
@@ -270,19 +270,27 @@ hl.bind('SUPER + P', hl.dsp.window.pin(), {
 
 -- Audio
 
-hl.bind('XF86AudioRaiseVolume', hl.dsp.exec_cmd('wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2%+'), {
-	locked = true,
-	repeating = true,
-	description = 'Increase volume',
-})
+hl.bind(
+	'XF86AudioRaiseVolume',
+	hl.dsp.exec_cmd('swayosd-client --output-volume +5 --max-volume 100'),
+	{
+		locked = true,
+		repeating = true,
+		description = 'Increase volume',
+	}
+)
 
-hl.bind('XF86AudioLowerVolume', hl.dsp.exec_cmd('wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-'), {
-	locked = true,
-	repeating = true,
-	description = 'Decrease volume',
-})
+hl.bind(
+	'XF86AudioLowerVolume',
+	hl.dsp.exec_cmd('swayosd-client --output-volume -5 --max-volume 100'),
+	{
+		locked = true,
+		repeating = true,
+		description = 'Decrease volume',
+	}
+)
 
-hl.bind('XF86AudioMute', hl.dsp.exec_cmd('wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'), {
+hl.bind('XF86AudioMute', hl.dsp.exec_cmd('swayosd-client --output-volume mute-toggle'), {
 	locked = true,
 	description = 'Toggle output mute',
 })
@@ -294,13 +302,13 @@ hl.bind('XF86AudioMicMute', hl.dsp.exec_cmd('wpctl set-mute @DEFAULT_AUDIO_SOURC
 
 -- Display brightness
 
-hl.bind('XF86MonBrightnessUp', hl.dsp.exec_cmd('brightnessctl -e4 -n2 set 5%+'), {
+hl.bind('XF86MonBrightnessUp', hl.dsp.exec_cmd('swayosd-client --brightness +5'), {
 	locked = true,
 	repeating = true,
 	description = 'Increase brightness',
 })
 
-hl.bind('XF86MonBrightnessDown', hl.dsp.exec_cmd('brightnessctl -e4 -n2 set 5%-'), {
+hl.bind('XF86MonBrightnessDown', hl.dsp.exec_cmd('swayosd-client --brightness -5'), {
 	locked = true,
 	repeating = true,
 	description = 'Decrease brightness',
